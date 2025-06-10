be9b1704579e903e37a0d70017f6caf58018b5c9
ariken <ariken12.nam@gmail.com>
2025-06-10 16:46:21 +0300

    fix for python3.11

--- structure/Requests/ShowList.py
+++ structure/Requests/ShowList.py
@@ -11,6 +11,6 @@ class ShowList(CommandHandler):
 
     async def __handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         message = '''
-Ваш список поисковых запросов:\n''' + '\n'.join([f'{i+1}. {query} за {item['price']} руб. ({"Найдено" if item["founded"] else "Не найдено"})'
+Ваш список поисковых запросов:\n''' + '\n'.join([f'{i+1}. {query} за {item["price"]} руб. ({"Найдено" if item["founded"] else "Не найдено"})'
                                              for i, (query, item) in enumerate(self.core.get_subs(update.effective_chat.id).items())])
         await context.bot.send_message(chat_id=update.effective_chat.id, text=message, reply_markup=MENU_MARKUP)
